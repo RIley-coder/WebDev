c8a64fc21ee6e664948e84569ec3902644b141e6
RIley-coder <60401358+RIley-coder@users.noreply.github.com>
2020-04-07 13:26:38 -0500

    Riley- coder

--- Version9.0/user21/Ebay.php
+++ Version9.0/user21/Ebay.php
@@ -4,9 +4,9 @@ error_reporting(E_ALL);  // Turn on all errors, warnings and notices for easier 
 // API request variables
 $endpoint = 'http://svcs.ebay.com/services/search/FindingService/v1';  // URL to call
 $version = '1.0.0';  // API version supported by your application
-$appid = 'KEY HERE';  // Replace with your own AppID
+$appid = 'RobertMa-Shakopee-PRD-169ec6b8e-bb30ba02';  // Replace with your own AppID
 $globalid = 'EBAY-US';  // Global ID of the eBay site you want to search (e.g., EBAY-DE)
-$query = 'screwdrivers';  // You may want to supply your own query
+$query = 'Tools';  // You may want to supply your own query
 $safequery = urlencode($query);  // Make the query URL-friendly
 $i = '0';  // Initialize the item filter index to 0
 // Create a PHP array of the item filters you want to use in your request
@@ -100,13 +100,95 @@ else {
         }
 
     </style>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="5.4 Final Project" content="Riley's Amzing Website">
+
+    <title>Web Dev User #21 </title>
+
+    <!-- Bootstrap core JS -->
+    <!-- These are needed to get the responsive menu to work -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+
+    <!-- Custom styles for this template -->
+    <style type="text/css">
+        .menu {
+            margin: 0px;
+        }
+
+        .wideMargin {
+            margin: 15px;
+        }
+
+        #footer {
+            font-size: 12px;
+            text-align: center;
+        }
+
+        td {
+            color: #FF0000;
+        }
+
+    </style>
+    <style>
+        p {
+            padding: 20px;
+            font: 20px sans-serif;
+            background: khaki;
+        }
+
+    </style>
+    <script>
+        $(document).ready(function() {
+            $("p").click(function() {
+                $(this).slideUp();
+            });
+        });
+
+    </script>
+
+
+    <style>
 </head>
 
 <body>
+     <div class="menu">
+        <nav class="navbar navbar-expand-md navbar-dark bg-dark">
+            <a href="http://shakonet.isd720.com" class="navbar-brand">WebDev</a>
+            <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#navbarCollapse">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse" id="navbarCollapse">
+                <div class="navbar-nav">
+                    <!---------------------------------- Edit These Items in your Menu ------------->
+                    <a href="index.html" class="nav-item nav-link">Home</a>
+                    <a href="Script.html" class="nav-item nav-link">Games</a>
+                    <a href="music.html" class="nav-item nav-link" tabindex="-1">Music</a>
+                    <a href="List.html" class="nav-item nav-link" tabindex="-1">Lists</a>
+                    <a href="AboutMe.html" class="nav-item nav-link">About Me</a>
+                    <a href="wheresMyStudent.html" class="nav-item nav-link">Hall Pass</a>
+                    <a href="Ebay.php" class="nav-item nav-link active">Ebay</a>
+                    <a href="mailto:sample@gmail.com?Subject=Hello" class="nav-item nav-link" tabindex="-2">Contacts</a>
+                    <!----------------------------------^ Edit These Items in your Menu ^ ------------->
+                </div>
+                <div class="navbar-nav ml-auto">
+                    <a href="#" class="nav-item nav-link disabled">Login</a>
+                </div>
+            </div>
+        </nav>
+    </div>
 
     <h1>eBay Search Results for <?php echo $query; ?></h1>
 
-    <table>
+    <table border='13'>
         <tr>
             <td>
                 <?php echo $results;?>
